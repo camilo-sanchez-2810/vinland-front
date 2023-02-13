3551a9588b245f7b587f7e42032d771aa12b9556
valencaceres <vcaceres102@gmail.com>
2023-02-13 19:54:49 -0300

    cosas arregladas xd

--- src/views/Shopping/Shopping.jsx
+++ src/views/Shopping/Shopping.jsx
@@ -105,6 +105,7 @@ export default function Shopping() {
                 <div className={style.buttons}>
                     {boton2()}
                     {boton()}
+                    
                 </div>
             </div>
         </div>
