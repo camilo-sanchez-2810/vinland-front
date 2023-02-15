84ca62fd174971530cc271291f02b7c649ba8e0a
laptinho <camsanbar@gmail.com>
2023-02-15 18:38:29 -0300

    TODO: arreglar estado del carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './cart.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = ({show, handleShow}) => {
   return (
@@ -11,8 +11,38 @@ const Cart = ({show, handleShow}) => {
           <span className={styles.cartTitle}>Carrito</span>
           <button className={styles.closeCart} onClick={handleShow}><FontAwesomeIcon icon={faXmark}/></button>
         </div>
-        <div>
-          <p>No hay productos en el carrito</p>
+        <div className={styles.products}>
+          {/* {<p className={styles.emptyMessage}>No hay productos en el carrito</p>} */}
+          <div className={styles.product}>
+            <div>
+              <img className={styles.productImg} src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/512SHoBt4YL.jpg" alt="Producto" />
+            </div>
+            <div className={styles.productBody}>
+              <p className={styles.productTitle}>Nombre producto</p>
+              <div className={styles.productQuantity}>
+                <div className={styles.quantityContent}>
+                  <button className={styles.quantityButtons}><FontAwesomeIcon icon={faPlus}/></button>
+                  <span>1</span>
+                  <button className={styles.quantityButtons}><FontAwesomeIcon icon={faMinus}/></button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.product}>
+            <div>
+              <img className={styles.productImg} src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/512SHoBt4YL.jpg" alt="Producto" />
+            </div>
+            <div className={styles.productBody}>
+              <p className={styles.productTitle}>Nombre producto</p>
+              <div className={styles.productQuantity}>
+                <div className={styles.quantityContent}>
+                  <button className={styles.quantityButtons}><FontAwesomeIcon icon={faPlus}/></button>
+                  <span>1</span>
+                  <button className={styles.quantityButtons}><FontAwesomeIcon icon={faMinus}/></button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/store/Cart/reducers.js
+++ src/store/Cart/reducers.js
@@ -3,28 +3,22 @@ import { createReducer } from '@reduxjs/toolkit'
 
 const { addProduct } = cartActions
 
-const initialState = {
-  products: [],
-  total: 0
-}
-
+const initialState = []
 const cartReducers = createReducer(initialState, (builder) => {
   builder
     .addCase(addProduct, (state, action) => {
-      const exist = state.products.some(product => product.product_id === action.payload)
-      let newState = {}
-      if(!exist) {
-        newState = {
-          products: [...state.products, { product_id: action.payload, quantity: 1 }],
-          total: state.total
-        }
+      const exist = state.some(product => product.product_id === action.payload)
+      console.log(state)
+      let newState = []
+      if(exist) {
+        newState = state.map((product) => {
+          if(product.product_id === action.payload) {
+            return {...product, quantity: product.quantity + 1}
+          }
+          return product
+        })
       } else {
-        newState = {
-          products: state.products.map((product) => {
-            return product.product_id === action.payload ? ({...product, quantity: product.quantity +1}) : product
-          }),
-          total: state.total
-        }
+        newState = [...state, {product_id: action.payload, quantity: 1}]
       }
       return newState
     })
