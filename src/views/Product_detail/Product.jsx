b9992bb606120e760ceaa30c77a8131772776b09
Satch5918 <satch_elfante@hotmail.com>
2023-02-20 16:09:39 -0300

    tienda

--- src/views/Product_detail/Product.jsx
+++ src/views/Product_detail/Product.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import productActions from '../../store/Product/actions'
 import axios from 'axios'
 import { Link as Anchor } from 'react-router-dom'
+import Swal from 'sweetalert2'
 /* import YouTube from "react-youtube" */
 const {get_one} = productActions
 
@@ -26,6 +27,30 @@ export default function Product() {
     },[])
     console.log(product);
 
+    const description = () => {
+        Swal.fire({
+            title: 'Descripcion',
+            text: `${product.description}`,
+            imageUrl: `${product.photo}`,
+            imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+          })
+      };
+
+      const demo = () => {
+        Swal.fire({
+            title: `${product.name}`,
+            html:
+              `<iframe width="400" height="300" src="https://www.youtube.com/embed/${product.demo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+            imageWidth: 400,
+            imageHeight: 300,
+            imageAlt: 'Custom image',
+          })
+        
+        
+       
+      }
 /*     const opts = {
         height: '50',
         width: '300',
@@ -39,32 +64,51 @@ export default function Product() {
   return (
     <>
     <div className={style.container}>
-        <div>
             <div className={style.vinylbg}>
                 <Anchor className={style.back} to={'/shopping'}><img src='/assets/images/arrow-black.png' className={style.anchorImg}/></Anchor>
             </div>
             <div className={style.cardContainer}>
-                <div className={style.card}>
-                    <div className={style.imgContainer}>
-                        <img className={style.img} src={product.photo} alt="" />
-                        <h3>{product.name}</h3>
-                        {/* <audio preload="true" controls src='/assets/audio/nunca_quise.mp3' className={style.audio}/> */}
-                        {/* <YouTube className={style.youtube} videoId={'GL56LY6fE0E'} opts={opts}/> */}
+                <div className={style.imgCont}>
+                <img className={style.img} src={product.photo} alt="" />
+                </div>
+                <div className={style.textCont}>
+                    <h3 className={style.h31}>{product.artist}</h3>
+                    <h3 className={style.h32}>{product.name}</h3>
+                    <button onClick={description} className={style.cart}>Descripcion</button>
+                    <button onClick={demo} className={style.cart}>DEMO</button>
+                    <div className={style.carritoCOnt}>
+                    <img className={style.vinImg} src="/assets/images/vinilo.png" alt="" />
+                    
+                    <button className={style.cartbut}> <img  className={style.cartImg} src="/assets/images/cart.png" alt="" /></button>
                     </div>
-                    <div className={style.textContainer}>
-                        <p className={style.text}>{product.description}</p>
-                        <div className={style.priceContainer}>
-                            <p className={style.price}>{product.price}$</p>
-                            <button className={style.cart}>Añadir al carrito</button>
-                        </div>
-                    </div>
+                    
+                    <p className={style.price}>{product.price}$</p>
+                    
                 </div>
             </div>
-            <div className={style.brown}>
-
-            </div>
-        </div>
     </div>
     </>
   )
 }
+
+
+//<div className={style.imgContainer}>
+  //                      <img className={style.img} src={product.photo} alt="" />
+    //                </div>
+      //              <div className={style.textContainer}>
+        //                <div className={style.h3Container}>
+          //                  <h3 className={style.h31}>{product.artist}</h3>
+            //                 <h3 className={style.h32}>{product.name}</h3>
+            //          </div>
+             
+            
+                        {/* <p className={style.text}>{product.description}</p> */}
+            //            <div className={style.priceContainer}>
+              //              <div className={style.asdContainer}> 
+                //                <img className={style.vinImg} src="/assets/images/vinilo.png" alt="" />
+              //                  <p className={style.price}>{product.price}$</p></div>
+                //           <div>
+                  //          <button className={style.cart}>Añadir al carrito</button>
+                    //        </div>
+                      //  </div>
+                    //</div>
